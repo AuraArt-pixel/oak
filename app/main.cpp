00b64e504bb2d6d25a733ab961002dcf56fe085e
AuraArt-pixel <auralustart@proton.me>
2026-07-21 00:17:09 +0530

    Fix: main.cpp AddOption→add_option, IsSet→is_set

--- app/main.cpp
+++ app/main.cpp
@@ -213,7 +213,7 @@ int main(int argc, char *argv[])
 							 "main", "Decompress project file (No GUI)"));
 
 #ifdef _WIN32
-	auto console_option = parser.AddOption(
+	auto console_option = parser.add_option(
 		{ QStringLiteral("c"), QStringLiteral("-console") },
 		QCoreApplication::translate("main", "Launch with debug console"));
 #endif // _WIN32
@@ -331,7 +331,7 @@ int main(int argc, char *argv[])
 #ifdef _WIN32
 		// Since Oak Video Editor is linked with the console subsystem (for better POSIX compatibility), a console
 		// is created by default. If the user didn't request one, we free it here.
-		if (!console_option->IsSet()) {
+		if (!console_option->is_set()) {
 			FreeConsole();
 		}
 #endif // _WIN32
